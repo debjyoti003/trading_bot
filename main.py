033cad31f05657f14a72dbabfc4397ca1c4805e2
Debjyoti Banerjee <debjyoti003@debjyoti.banerjee1994@gmail.com>
2023-11-09 21:28:37 +0530

    coding with memory added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import pickle
-import streamlit as st
 import os
+import streamlit as st
+from streamlit_chat import message
 from config import OPENAI_API_KEY
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain.memory import ConversationBufferMemory
 from langchain.chains.question_answering import load_qa_chain
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
@@ -14,34 +16,27 @@ st.set_page_config(
     page_icon=":robot_face",
 )
 
-st.title("Trading Chatbot")
+st.header("Custom Trading Chatbot")
 
-openai_llm = ChatOpenAI(streaming = True, temperature = 0, model_name = 'ft:gpt-3.5-turbo-0613:personal::8FeSw7Cz')
+# openai_llm = ChatOpenAI(streaming = True, temperature = 0, model_name = 'ft:gpt-3.5-turbo-0613:personal::8FeSw7Cz')
+llm = ChatOpenAI(streaming = True, temperature = 0, model_name = 'gpt-3.5-turbo')
 
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [
+        SystemMessage(content="You are a coding expert who is fluent and writes error free code in different programming languages")
+    ]
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+user_input = st.text_input("Your message: ", key = "user_input")
 
-if prompt := st.chat_input("Please ask your question"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if user_input:
+    st.session_state.messages.append(HumanMessage(content=user_input))
+    with st.spinner("Thinking..."):
+        response = llm(st.session_state.messages)
+    st.session_state.messages.append(AIMessage(content=response.content))
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for message in st.session_state.messages:
-            try:
-                llm = ChatOpenAI(streaming = True, temperature = 0, model_name = 'gpt-3.5-turbo')
-                template = """Consider yourself as a Trading expert, and you need to answer the given question accordinly: {question}"""
-                prompt = PromptTemplate(template=template, input_variables=["question"])
-                llm_chain = LLMChain(prompt=prompt, llm=openai_llm)
-                # chain = load_qa_chain(llm = openai_llm, chain_type = "stuff")
-                response = llm_chain.run(message['content'])
-            except:
-                response = st.error("OpenAI model currently is overloaded with request, please try to ask your question again")
-        message_placeholder.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+messages = st.session_state.get('messages', [])
+for i, msg in enumerate(messages[1:]):
+    if i % 2 == 0:
+        message(msg.content, is_user=True, key=str(i) + '_user')
+    else:
+        message(msg.content, is_user=False, key=str(i) + '_ai')
